0999721080a3e86842fcac532b79f42e4d92218c
Ricardo Steijn <ricardo.steijn97@gmail.com>
2021-08-09 00:53:26 +0200

    Bump version to 2.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='crownstone-sse',
-    version="2.0.0",
+    version="2.0.1",
     url='https://github.com/crownstone/crownstone-lib-python-sse',
     author='Crownstone B.V.',
     long_description=long_description,
